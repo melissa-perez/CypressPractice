d8f324807c19f9c2e38541bc4ed7710809a207a3
Melissa Perez <melmperz@gmail.com>
2022-06-20 19:03:43 -0400

    Add second and third tests to suite

--- cypress/e2e/firstTest.cy.ts
+++ cypress/e2e/firstTest.cy.ts
@@ -4,7 +4,96 @@
 describe('Our first suite', () => {
 
     it('first test', () => {
+        // out route path
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        // by tag name
+        cy.get('input')
+
+        // by id
+        cy.get('#inputEmail1')
+
+        // by class name
+        cy.get('.input-full-width')
+
+        // by attribute name
+        cy.get('[placeholder]')
+
+        // by attribute name and value
+        cy.get('[placeholder="Email"]')
+
+        // by class value, need to provide the entire class and value
+        cy.get('[class="input-full-width size-medium shape-rectangle"]')
+
+        // by tag name and attribute with value
+        cy.get('input[placeholder="Email"]')
+
+        // by two different attributes
+        cy.get('input[placeholder="Email"][type="email"]')
+
+        // by tag name, attribute with value, ID and class name
+        cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+
+        // the most recommended way by Cypress, add my own attribute dynamically
+        cy.get('[data-cy="imputEmail1"]')
+
+
 
     })
+
+    it('second test', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.get('[data-cy="signInButton"]')
+        cy.contains('Sign in')
+
+        cy.contains('[status="warning"]','Sign in')
+        // find specific button within this form
+        cy.get('#inputEmail3').parents('form').find('button').should('contain', 'Sign in').parents('form').find('nb-checkbox').click()
+        // find the form then go into nb card with type email
+        cy.contains('nb-card', 'Horizontal form').find('[type="email"]')
+    })
+
+    it.only('third test then and wrap',  () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        // using the grid form
+        cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
+        cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
+
+        // basic form
+        cy.contains('nb-card', 'Basic form').find('[for="exampleInputEmail1"]').should('contain', 'Email')
+        cy.contains('nb-card', 'Basic form').find('[for="exampleInputPassword1"]').should('contain', 'Password')
+
+        //cannot save like this in cypress like then
+
+        // const gridForm = cy.contains('nb-card', 'Using the Grid')
+        // const basicForm =  cy.contains('nb-card', 'Basic form')
+
+        // gridForm.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
+        // gridForm.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
+
+
+        // basicForm.contains('nb-card', 'Basic form').find('[for="exampleInputEmail1"]').should('contain', 'Email')
+        // basicForm.contains('nb-card', 'Basic form').find('[for="exampleInputPassword1"]').should('contain', 'Password')
+
+        cy.contains('nb-card', 'Using the Grid').then(firstForm => {
+            const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text()
+            const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
+
+            expect(emailLabelFirst).to.equal('Email')
+            expect(passwordLabelFirst).to.equal('Password')
+
+        })
+
+    })
+
+    
 
 })
